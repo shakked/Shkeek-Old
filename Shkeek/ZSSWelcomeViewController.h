dad8cfd1f617bee54e7147a92f8488f0c2312d3b
shakked <zshakked@stevens.edu>
2015-02-08 15:12:51 -0500

    Added scrollview to home and social login buttons to welcome view

---
+++ Shkeek/ZSSWelcomeViewController.h
@@ -0,0 +1,13 @@
+//
+//  ZSSWelcomeViewController.h
+//  
+//
+//  Created by Zachary Shakked on 2/8/15.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ZSSWelcomeViewController : UIViewController
+
+@end
---
+++ Shkeek/ZSSWelcomeViewController.m
@@ -0,0 +1,31 @@
+//
+//  ZSSWelcomeViewController.m
+//  
+//
+//  Created by Zachary Shakked on 2/8/15.
+//
+//
+
+#import "ZSSWelcomeViewController.h"
+
+@interface ZSSWelcomeViewController ()
+
+@end
+
+@implementation ZSSWelcomeViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+    UIScrollView *scrollView = [[UIScrollView alloc] initWithFrame:self.view.frame];
+    [self.view addSubview:scrollView];
+    
+    
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
