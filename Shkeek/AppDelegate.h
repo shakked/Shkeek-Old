10e1d0ed7ebc83ebc0dd7fd0f6c21e1d25bc5d2a
shakked <zshakked@stevens.edu>
2015-02-08 13:18:59 -0500

    Initial commit

---
+++ Shkeek/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Shkeek
+//
+//  Created by Zachary Shakked on 2/8/15.
+//  Copyright (c) 2015 Shkeek Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
